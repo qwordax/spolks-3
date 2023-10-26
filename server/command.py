17751a4f5ad558e1d302a5aa4e78e1723e41ec09
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-26 12:47:48 +0300

    `feat!:` implement basic server

---
+++ server/command.py
@@ -0,0 +1,149 @@
+import logging
+import os
+import socket
+import time
+
+FATAL = False
+
+last_address = None
+last_file_name = None
+
+BUFSIZE = 1024
+
+def server_echo(conn, args):
+    response = '\n'.join(args[1:]) + '\n'
+    conn.send(response.encode('ascii'))
+
+def server_time(conn):
+    response = time.ctime() + '\n'
+    conn.send(response.encode('ascii'))
+
+def server_upload(conn, address):
+    global FATAL
+
+    global last_address
+    global last_file_name
+
+    file_info = conn.recv(BUFSIZE).decode('ascii').split()
+
+    file_name = file_info[0]
+    file_size = int(file_info[1])
+
+    is_continue = (last_address is not None and
+                   last_address == address[0] and
+                   last_file_name == file_name and
+                   FATAL is True)
+
+    if is_continue:
+        current_size = os.path.getsize(file_name)
+        file_mode = 'ab'
+    else:
+        current_size = 0
+        file_mode = 'wb'
+
+    conn.send(str(current_size).encode('ascii'))
+
+    logging.info('uploading . . .')
+
+    FATAL = False
+
+    last_address = address[0]
+    last_file_name = file_name
+
+    with open(file_name, file_mode) as file:
+        i = 0
+        oob = file_size // 1024 // 4
+
+        size = 0
+        oob_size = 0
+
+        while (current_size + size + oob_size) < file_size:
+            if i < oob:
+                conn.setsockopt(socket.SOL_SOCKET, socket.SO_OOBINLINE, 1)
+                oob_size += file.write(conn.recv(BUFSIZE))
+                conn.setsockopt(socket.SOL_SOCKET, socket.SO_OOBINLINE, 0)
+            else:
+                size += file.write(conn.recv(BUFSIZE))
+
+            full_size = current_size+size+oob_size
+
+            if i % (2*BUFSIZE) == 0:
+                logging.info(f'{int(100*full_size/file_size):3d} %')
+
+            i += 1
+
+        logging.info('100 %')
+        logging.info(f'received {size:,.0f} + {oob_size:,.0f} bytes')
+        logging.info(f'uploaded \'{file_name}\'')
+
+def server_download(conn, address, args):
+    global FATAL
+
+    global last_address
+    global last_file_name
+
+    if not os.path.exists(args[1]):
+        conn.send('not exists'.encode('ascii'))
+        return
+
+    file_name = args[1]
+    file_size = os.path.getsize(args[1])
+
+    is_continue = (last_address is not None and
+                   last_address == address[0] and
+                   last_file_name == file_name and
+                   FATAL is True)
+
+    if is_continue:
+        conn.send('continue'.encode('ascii'))
+    else:
+        conn.send('exists'.encode('ascii'))
+
+    file_info = file_name + ' ' + str(file_size)
+    conn.send(file_info.encode('ascii'))
+
+    current_size = int(conn.recv(BUFSIZE).decode('ascii'))
+
+    logging.info('downloading . . .')
+
+    FATAL = False
+
+    last_address = address[0]
+    last_file_name = file_name
+
+    with open(file_name, mode='rb') as file:
+        file.seek(current_size)
+
+        i = 0
+        oob = file_size // 1024 // 4
+
+        size = 0
+        oob_size = 0
+
+        for data in iter(lambda: file.read(BUFSIZE), b''):
+            if i < oob:
+                conn.setsockopt(socket.SOL_SOCKET, socket.SO_OOBINLINE, 1)
+                conn.send(data)
+                conn.setsockopt(socket.SOL_SOCKET, socket.SO_OOBINLINE, 0)
+
+                oob_size += len(data)
+            else:
+                conn.send(data)
+                size += len(data)
+
+            full_size = current_size+size+oob_size
+
+            if i % (2*BUFSIZE) == 0:
+                logging.info(f'{int(100*full_size/file_size):3d} %')
+
+            i += 1
+
+        logging.info('100 %')
+        logging.info(f'transmitted {size:,.0f} + {oob_size:,.0f} bytes')
+        logging.info(f'downloaded \'{file_name}\'')
+
+def server_unknown(conn, args):
+    logging.error(f'unknown command \'{" ".join(args)}\'')
+
+    response = f'error: unknown command \'{" ".join(args)}\'\n'
+    conn.send(response.encode('ascii'))
---
+++ server/main.py
@@ -0,0 +1,89 @@
+import logging
+import socket
+import sys
+
+import command
+
+def main():
+    if len(sys.argv) != 3:
+        print(f'usage: {sys.argv[0]} <address> <port>')
+        return
+
+    address = sys.argv[1]
+    port = int(sys.argv[2])
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(levelname)-8s : %(message)s'
+        )
+
+    socket.setdefaulttimeout(30)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.bind((address, port))
+
+    sock.listen(1)
+
+    working = True
+    timeout = 0
+
+    while working and timeout < 3:
+        logging.info('accepting . . .')
+
+        try:
+            conn, address = sock.accept()
+            logging.info(f'accepted {address[0] + ":" + str(address[1])}')
+        except TimeoutError:
+            logging.info('timeout'); timeout += 1
+            continue
+
+        try:
+            while True:
+                args = conn.recv(command.BUFSIZE).decode('ascii').split()
+
+                if args[0] == 'close':
+                    working = False
+                    break
+
+                if args[0] == 'exit' or args[0] == 'quit':
+                    break
+
+                logging.info(' '.join(args))
+
+                if args[0] == 'echo':
+                    command.server_echo(conn, args)
+                elif args[0] == 'time':
+                    command.server_time(conn)
+                elif args[0] == 'upload':
+                    command.server_upload(conn, address)
+                elif args[0] == 'download':
+                    command.server_download(conn, address, args)
+                else:
+                    command.server_unknown(conn, args)
+        except ConnectionAbortedError:
+            command.FATAL = True
+
+            logging.critical(
+                f'connection aborted {address[0]+":"+str(address[1])}')
+        except ConnectionResetError:
+            command.FATAL = True
+
+            logging.critical(
+                f'connection reset {address[0]+":"+str(address[1])}')
+        except TimeoutError:
+            command.FATAL = True
+
+            logging.critical(
+                f'timeout {address[0]+":"+str(address[1])}')
+        finally:
+            logging.info(
+                f'closed {address[0]+":"+str(address[1])}')
+
+            conn.close()
+
+    logging.info('closing . . .')
+    sock.close()
+
+if __name__ == "__main__":
+    main()
