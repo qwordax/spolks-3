3b6a481e0e08c7b39ab2f5458f5575eb9ad48c6b
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-10-31 13:38:44 +0300

    `feat!:` implement `upload` command

--- client/udp.py
+++ client/udp.py
@@ -1,3 +1,5 @@
+import os
+
 BUFSIZE = 1024
 
 def client_echo(sock, address, args):
@@ -13,7 +15,47 @@ def client_time(sock, address, args):
     print(sock.recvfrom(BUFSIZE)[0].decode('ascii'), end='')
 
 def client_upload(sock, address, args):
-    pass
+    if len(args) != 2:
+        print('usage: upload <file>')
+        return
+
+    if not os.path.exists(args[1]):
+        print(f'error: \'{args[1]}\' does not exists')
+        return
+
+    sock.sendto(' '.join(args).encode('ascii'), address)
+
+    file_name = args[1]
+    file_size = os.path.getsize(args[1])
+
+    file_info = file_name+' '+str(file_size)
+    sock.sendto(file_info.encode('ascii'), address)
+
+    current_size = int(sock.recv(BUFSIZE).decode('ascii'))
+
+    print(f'uploading \'{file_name}\'')
+
+    with open(file_name, 'rb') as file:
+        file.seek(current_size)
+
+        i = 0
+        size = 0
+
+        for data in iter(lambda: file.read(BUFSIZE), b''):
+            sock.sendto(int.to_bytes(i+1, length=4)+data, address)
+
+            size += len(data)
+            full_size = current_size+size
+
+            if i % (2*BUFSIZE) == 0:
+                print(f'{int(100*full_size/file_size):2d} %')
+
+            i += 1
+
+        sock.sendto(int.to_bytes(0, length=4)+bytes(BUFSIZE), address);
+
+        print(f'transmitted {size:,.0f} bytes')
+        print(f'uploaded \'{file_name}\'')
 
 def client_download(sock, address, args):
     pass
--- server/udp.py
+++ server/udp.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 
 FATAL = False
@@ -17,7 +18,68 @@ def server_time(sock, address):
     sock.sendto(response.encode('ascii'), address)
 
 def server_upload(sock, address):
-    pass
+    global FATAL
+
+    global last_address
+    global last_file_name
+
+    file_info = sock.recv(BUFSIZE).decode('ascii').split()
+
+    file_name = file_info[0]
+    file_size = int(file_info[1])
+
+    is_continue = (last_address is not None and
+                   last_address == address[0] and
+                   last_file_name == file_name and
+                   FATAL is True)
+
+    if is_continue:
+        current_size = os.path.getsize(file_name)
+        file_mode = 'ab'
+    else:
+        current_size = 0
+        file_mode = 'wb'
+
+    sock.sendto(str(current_size).encode('ascii'), address)
+
+    logging.info(f'uploading \'{file_name}\'')
+
+    FATAL = False
+
+    last_address = address[0]
+    last_file_name = file_name
+
+    i = 0
+    size = 0
+
+    file_dict = dict()
+
+    while True:
+        data = sock.recv(BUFSIZE+4)
+
+        index = int.from_bytes(data[:4])
+        data = data[4:]
+
+        if index == 0:
+            break
+
+        file_dict[index] = data
+
+        size += len(data)
+        full_size = current_size+size
+
+        if i % (2*BUFSIZE) == 0:
+            logging.info(f'{int(100*full_size/file_size):2d} %')
+
+        i += 1
+
+    logging.info(f'received {size:,.0f} bytes')
+
+    with open(file_name, file_mode) as file:
+        for _, data in sorted(file_dict.items()):
+            file.write(data)
+
+    logging.info(f'uploaded \'{file_name}\'')
 
 def server_download(sock, address, args):
     pass
